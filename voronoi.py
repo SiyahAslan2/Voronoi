625a59adeffd9f442e6797bf2c2bd98a2226cd69
SiyahAslan <kristianidzan@web.de>
2019-05-15 10:53:48 +0200

    test

--- voronoi.py
+++ voronoi.py
@@ -9,59 +9,77 @@ import numpy as np
 import warnings
 
 from scipy.spatial import cKDTree as KDTree
-from skimage.filters.rank import entropy
-from skimage.morphology import disk, dilation
+from skimage.filters.rank import entropy, otsu
+from skimage.morphology import disk, dilation, erosion, diamond
 from skimage.util import img_as_ubyte
 from skimage.io import imread, imsave
-from skimage.color import rgb2gray, rgb2lab, lab2rgb
-from skimage.filters import sobel, gaussian
-from skimage.restoration import denoise_bilateral
+from skimage.color import rgb2lab, lab2rgb
+from skimage.filters import sobel, gaussian, scharr
+from skimage.restoration import denoise_tv_bregman
 from skimage.transform import downscale_local_mean
 
-# test
-def rand(x):
+"""def rand(x):
     r = x
     while r == x:
         r = random.uniform(0, x)
-    return r
+    return r"""
+
+
+def uniform(a, b):
+    return a + (b - a) * random.random()
 
 
 def poisson_disc(img, n, k=30):
     h, w = img.shape[:2]
 
-    nimg = denoise_bilateral(img, sigma_spatial=15, multichannel = True)
+    nimg = denoise_tv_bregman(img, weight=1)
+
+    def rgb2gray(rgb):
+        return np.dot(rgb[:, :, :3], [0.2989, 0.5870, 0.1140])
+
     img_gray = rgb2gray(nimg)
     img_lab = rgb2lab(nimg)
 
-    entropy_weight = 2**(entropy(img_as_ubyte(img_gray), disk(15)))
-    entropy_weight /= np.amax(entropy_weight)
-    entropy_weight = gaussian(dilation(entropy_weight, disk(15)), 5)
+    """entropy_weight = 2**(entropy(img_as_ubyte(img_gray), disk(15))) # da Entropy log zur Basis 2
+    entropy_weight /= np.amax(entropy_weight) # max. des arrays
+    entropy_weight = gaussian(dilation(entropy_weight, disk(15)), 5)"""
 
-    color = [sobel(img_lab[:, :, channel])**2 for channel in range(1, 3)]
-    edge_weight = functools.reduce(op.add, color) ** (1/2) / 75
+    """otsu_weight = otsu(img_as_ubyte(img_gray), disk(15))
+    otsu_weight = gaussian(dilation(otsu_weight, disk(15)), 5)"""
+
+    # color = [scharr(img_lab[:, :, i])**2 for i in range(1, 3)]
+    color = []
+    for i in range(1, 3):
+        color.append(scharr(img_lab[:, :, i]) ** 2)
+    edge_weight = functools.reduce(lambda x, y: x + y, color) ** (1 / 2) / 50
     edge_weight = dilation(edge_weight, disk(5))
 
-    weight = (0.3*entropy_weight + 0.7*edge_weight)
+    weight = edge_weight
     weight /= np.mean(weight)
-    weight = weight
 
-    max_dist = min(h, w) / 4
+    max_dist = avg_dist * 8  # min(h, w) / 4
     avg_dist = math.sqrt(w * h / (n * math.pi * 0.5) ** (1.05))
     min_dist = avg_dist / 4
 
     dists = np.clip(avg_dist / weight, min_dist, max_dist)
 
+    # Generate first point randomly.
+    first_point = (uniform(0, h), uniform(0, w))
+    to_process = [first_point]
+    sample_points = [first_point]
+    tree = KDTree(sample_points)
+
     def gen_rand_point_around(point):
-        radius = random.uniform(dists[int(point[0])][int(point[1])], max_dist)
-        angle = rand(2 * math.pi)
+        radius = uniform(dists[int(point[0])][int(point[1])], max_dist)
+        angle = uniform(0, 2 * math.pi)
         offset = np.array([radius * math.sin(angle), radius * math.cos(angle)])
         return tuple(point + offset)
 
     def has_neighbours(point):
-        point_dist = dists[int(point[0])][int(point[1])]
+        point_dist = dists[int(point[0])][int(point[1])]  # Output: int
         distances, idxs = tree.query(point,
-                                    len(sample_points) + 1,
-                                    distance_upper_bound=max_dist)
+                                     len(sample_points) + 1,
+                                     distance_upper_bound=max_dist)
 
         if len(distances) == 0:
             return True
@@ -74,12 +92,6 @@ def poisson_disc(img, n, k=30):
                 return True
 
         return False
-
-    # Generate first point randomly.
-    first_point = (rand(h), rand(w))
-    to_process = [first_point]
-    sample_points = [first_point]
-    tree = KDTree(sample_points)
 
     while to_process:
         # Pop a random point.
@@ -105,7 +117,7 @@ def sample_colors(img, sample_points, n):
     h, w = img.shape[:2]
 
     print("Sampling colors...")
-    tree = KDTree(np.array(sample_points))
+    tree = KDTree(np.array(sample_points))  # divide into voronoi segments
     color_samples = collections.defaultdict(list)
     img_lab = rgb2lab(img)
     xx, yy = np.meshgrid(np.arange(h), np.arange(w))
@@ -146,14 +158,14 @@ def sample_colors(img, sample_points, n):
 
 def render(img, color_samples):
     print("Rendering...")
-    h, w = [2*x for x in img.shape[:2]]
+    h, w = [2 * x for x in img.shape[:2]]
     xx, yy = np.meshgrid(np.arange(h), np.arange(w))
     pixel_coords = np.c_[xx.ravel(), yy.ravel()]
 
     colors = np.empty([h, w, 3])
     coords = []
     for color_sample in color_samples:
-        coord = tuple(x*2 for x in color_sample[:2][::-1])
+        coord = tuple(x * 2 for x in color_sample[:2][::-1])
         colors[int(coord[0])][int(coord[1])] = color_sample[2:]
         coords.append(coord)
 
@@ -166,24 +178,20 @@ def render(img, color_samples):
 
 
 if __name__ == "__main__":
-    warnings.simplefilter("ignore")
-
-    img = imread("Liverpool1.jpg")[:, :, :3]
-
-    print("Calibrating...")
+    # warnings.simplefilter("ignore")
+    img = imread("autorenders/Bielefeld.jpg")
+    print("Calibrating")
     mult = 1.02 * 500 / len(poisson_disc(img, 500))
 
-    for n in range(700, 701):
-        print("Sampling {} for size {}.".format("Planet.jpg", n))
+# n = int(input("Geben Sie ein n an:"))
+n = 1000
+sample_points = poisson_disc(img, mult * n)
+samples = sample_colors(img, sample_points, n)
+base = os.path.basename("Bielefeld.jpg")
+"""with open("{}-{}.txt".format(os.path.splitext(base)[0], n), "w") as f:
+    for sample in samples:
+        f.write(" ".join("{:.3f}".format(x) for x in sample) + "\n")"""
+imsave("autorenders/Voronoi/{}-{}.png".format(os.path.splitext(base)[0], n),
+       render(img, samples))
 
-        sample_points = poisson_disc(img, mult * n)
-        samples = sample_colors(img, sample_points, n)
-        base = os.path.basename("Planet.jpg")
-        with open("{}-{}.txt".format(os.path.splitext(base)[0], n), "w") as f:
-            for sample in samples:
-                f.write(" ".join("{:.3f}".format(x) for x in sample) + "\n")
-
-        imsave("autorenders/{}-{}.png".format(os.path.splitext(base)[0], n),
-            render(img, samples))
-
-        print("Done!")
+print("Done!")
